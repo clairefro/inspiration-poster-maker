18553c0bb479824174382d4a72d458e28ff9655f
claire froelich <claire.froelich@gmail.com>
2020-07-06 12:40:16 -0400

    Updates

---
+++ precache-manifest.d1abab3ec5b6bf8e1dc462fea87a7912.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef22669f9963f1a845707e2780cbe326",
+    "url": "/inspiration-poster-maker/index.html"
+  },
+  {
+    "revision": "4fa73b06c8ec8af3a5be",
+    "url": "/inspiration-poster-maker/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "cf0b45fac3823fa224d7",
+    "url": "/inspiration-poster-maker/static/js/2.38216a36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inspiration-poster-maker/static/js/2.38216a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa73b06c8ec8af3a5be",
+    "url": "/inspiration-poster-maker/static/js/main.5beaee8f.chunk.js"
+  },
+  {
+    "revision": "7b749a2283794a20c532",
+    "url": "/inspiration-poster-maker/static/js/runtime-main.dba5f4a3.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/inspiration-poster-maker/static/media/mintbean.cf18b629.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/inspiration-poster-maker/precache-manifest.d1abab3ec5b6bf8e1dc462fea87a7912.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/inspiration-poster-maker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
