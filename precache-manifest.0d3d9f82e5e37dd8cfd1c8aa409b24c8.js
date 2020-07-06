3c379dbd8f2dcc6e91870d12d62b19ec12c42822
claire froelich <claire.froelich@gmail.com>
2020-07-06 15:04:53 -0400

    Updates

---
+++ precache-manifest.0d3d9f82e5e37dd8cfd1c8aa409b24c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fb05e273439bbe9e09ba87a330dbfc",
+    "url": "/inspiration-poster-maker/index.html"
+  },
+  {
+    "revision": "c730d4fa7a7ebc3d9469",
+    "url": "/inspiration-poster-maker/static/css/main.164d6f35.chunk.css"
+  },
+  {
+    "revision": "c151361f0797a6230bd2",
+    "url": "/inspiration-poster-maker/static/js/2.73ac94a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inspiration-poster-maker/static/js/2.73ac94a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c730d4fa7a7ebc3d9469",
+    "url": "/inspiration-poster-maker/static/js/main.d5209935.chunk.js"
+  },
+  {
+    "revision": "7b749a2283794a20c532",
+    "url": "/inspiration-poster-maker/static/js/runtime-main.dba5f4a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/inspiration-poster-maker/precache-manifest.d1abab3ec5b6bf8e1dc462fea87a7912.js"
+  "/inspiration-poster-maker/precache-manifest.0d3d9f82e5e37dd8cfd1c8aa409b24c8.js"
 );
 
 self.addEventListener('message', (event) => {
